792866be580b09e4ba27aa193f24727788bec315
= <=>
2024-03-11 23:12:13 +0000

    this is my first commit

---
+++ src/test/java/ma/fsts/microserviceproject/MicroserviceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fsts.microserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
